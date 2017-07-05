a40459fd5b816bd6e39a73abf8287ff63bb658e2
David Rueda <davd33@gmail.com>
2017-07-05 19:06:55 +0200

    CircleCI 2 - init

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+configure_aws_cli() {
+	aws --version
+	aws configure set default.region eu-central-1
+	aws configure set default.output json
+}
+
+push_ecr_image() {
+	eval $(aws ecr get-login --region eu-central-1)
+	docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/lt-ms-wiki:$TAG
+}
+
+configure_aws_cli
+push_ecr_image
